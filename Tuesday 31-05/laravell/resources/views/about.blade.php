fe839bd0caf0ccd6fd62cd7eee3238e32c6cd7b0
roayas <100063786+roayas@users.noreply.github.com>
2022-05-31 20:45:35 +0300

    'task'

---
+++ Tuesday 31-05/laravell/resources/views/about.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/about.css') }}" >
+    <title>Document</title>
+    <style>
+ 
+
+.address-content {
+    font-size: 13px
+}
+
+.card {
+    height: 100%
+}
+
+.content-aboutme span {
+    font-size: 13px
+}
+
+.portfolio-content p {
+    font-size: 13px
+}
+
+.badge {
+    font-size: 11px;
+    font-weight: 600
+}
+        </style>
+</head>
+<body>
+    <div class="container mt-5 mb-5">
+        <div class="card">
+            <div class="row g-2">
+                <div class="col-md-4">
+                    <div class="p-3 text-center border-end"> <img class="rounded-circle" width="120" src="https://i.imgur.com/rvQ3LAt.jpg" />
+                        <h4 class="mt-2">Marry Longer</h4> <span class="d-flex justify-content-center align-items-center"><small class="margin-right">Location</small><i class="ml-1 fa fa-info-circle text-success"></i></span> <span class="address-content">Zone 5, Southeast, US</span>
+                        <div class="content-aboutme mt-3 p-3">
+                            <h1>About Me</h1> <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="p-3">
+                        <div class="d-flex justify-content-between align-items-center"> <span class="fw-bold text-success">Portfolio</span> <i class="fa fa-search-plus text-danger"></i> </div>
+                        <div class="d-flex gap-2 mt-2">
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/EEguU02.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/Uv2Yqzo.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/xbTAITF.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/EEguU02.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/xbTAITF.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/rvQ3LAt.jpg"> </div>
+                        </div>
+                        <div class="d-flex gap-2 mt-2">
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/EEguU02.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/rvQ3LAt.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/EEguU02.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/rvQ3LAt.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/rvQ3LAt.jpg"> </div>
+                            <div> <img class="rounded img-fluid" width="100" src="https://i.imgur.com/EEguU02.jpg"> </div>
+                        </div>
+                        <div class="portfolio-content mt-3">
+                            <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                        </div>
+                        <div class="mt-2">
+                            <h5>Area of Expertise</h5>
+                            <div class=""> <span class="badge bg-danger mb-1">Vegetable</span> <span class="badge bg-danger mb-1">Herbs</span> <span class="badge bg-danger mb-1">Dry Climate</span> <span class="badge bg-danger mb-1">Flowers</span> <span class="badge bg-danger mb-1">Fruits</span> <span class="badge bg-danger mb-1">Dry Fruits</span> <span class="badge bg-danger mb-1">Vegetable</span> <span class="badge bg-danger mb-1">Herbs</span> <span class="badge bg-danger mb-1">Dry Climate</span> <span class="badge bg-danger mb-1">Flowers</span> <span class="badge bg-danger mb-1">Fruits</span> <span class="badge bg-danger mb-1">Dry Fruits</span> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Tuesday 31-05/laravell/resources/views/roa.blade.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Stylish Portfolio - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Simple line icons-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}" >
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+        <nav id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand"><a href="#page-top">Start Bootstrap</a></li>
+                <li class="sidebar-nav-item"><a href="#page-top">Home</a></li>
+                <li class="sidebar-nav-item"><a href="#about">About</a></li>
+                <li class="sidebar-nav-item"><a href="#services">Services</a></li>
+                <li class="sidebar-nav-item"><a href="#portfolio">Portfolio</a></li>
+                <li class="sidebar-nav-item"><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <!-- Header-->
+        <header class="masthead d-flex align-items-center">
+            <div class="container px-4 px-lg-5 text-center">
+                <h1 class="mb-1">Candy Store</h1>
+                <h3 class="mb-5"><em>A Free Bootstrap Theme by Start Bootstrap</em></h3>
+                <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
+            </div>
+        </header>
+        <footer class="footer text-center">
+            <div class="container px-4 px-lg-5">
+                <ul class="list-inline mb-5">
+                    <li class="list-inline-item">
+                        <a class="social-link rounded-circle text-white mr-3" href="#!"><i class="icon-social-facebook"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="social-link rounded-circle text-white mr-3" href="#!"><i class="icon-social-twitter"></i></a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="social-link rounded-circle text-white" href="#!"><i class="icon-social-github"></i></a>
+                    </li>
+                </ul>
+                <p class="text-muted small mb-0">Copyright &copy; Your Website 2022</p>
+            </div>
+        </footer>
+</body>
+<script  src="{{ asset('js/scripts.js') }}"></script>
